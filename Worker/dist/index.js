a61e63dba17b8b81847dba65aa36d57d2c19be81
aayush <hiaayush30@gmail.com>
2025-09-01 11:34:17 +0530

    init

---
+++ Worker/dist/index.js
@@ -0,0 +1,10 @@
+import express from "express";
+const app = express();
+app.use(express.json());
+app.get("/", (req, res) => {
+    res.json("Hello There");
+});
+app.listen(process.env.PORT || 8000, () => {
+    console.log("server running on ", (process.env.PORT || 8000));
+});
+//# sourceMappingURL=index.js.map
